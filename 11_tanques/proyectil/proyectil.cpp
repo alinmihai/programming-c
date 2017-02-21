971f2129037ea0c4f5abfb6ad97200fc12d072a2
alinmihai <miguel.guceanu.1rh@gmail.com>
2017-02-21 19:21:59 +0100

    Modificaciones

--- 11_tanques/proyectil/proyectil.cpp
+++ 11_tanques/proyectil/proyectil.cpp
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DELTA 0,1
+#define DELTA 0.1
+#define MAXT 2
 #define ax 0
 #define ay -10
 
 int main (int argc, char *argv[]){
 
-    double ax, ay;
-    double v[DIM];
+    double vx = 20;
+    double vy = 40;
 
-    for (int i=0; )
+    for(double i=0.1; i<MAXT; i+=DELTA){
+        vx = vx + ax * DELTA;
+        printf("X: %.2lf", vx);
+        printf("\n");
+    }
+
+    for (double i=0.1; i<MAXT; i+=DELTA){
+        vy = vy + ay * DELTA;
+        printf("Y= %.2lf", vy);
+        printf("\n");
+    }
+
+    printf("\n\n");
 
 	return EXIT_SUCCESS;
 }
