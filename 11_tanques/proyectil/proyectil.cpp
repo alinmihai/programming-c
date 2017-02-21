cd5f3c7917635a75ba3a082a503b0e45a27fac65
Alin Mihai Guceanu <miguel.guceanu.1rh@gmail.com>
2017-02-21 12:58:40 +0100

    Modificaciones

---
+++ 11_tanques/proyectil/proyectil.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DELTA 0,1
+#define ax 0
+#define ay -10
+
+int main (int argc, char *argv[]){
+
+    double ax, ay;
+    double v[DIM];
+
+    for (int i=0; )
+
+	return EXIT_SUCCESS;
+}
