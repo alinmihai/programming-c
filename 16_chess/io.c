8941aac2bd0a11d682c484d7969d29d722d7bc05
Alin Mihai Guceanu <miguel.guceanu.1rh@gmail.com>
2017-04-20 14:23:38 +0200

    chess

---
+++ 16_chess/io.c
@@ -0,0 +1,83 @@
+#include "io.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <error.h>
+#include <errno.h>
+
+const char *good_chars = "TCAKQP-";
+
+void
+print_usage (FILE *output)
+{
+    fprintf(out, USAGE, program_invocation_short_name);
+}
+
+void
+error_ocurred ()
+{
+    printf_usage (stderr);
+    error (-errno, errno, "%s\n", strerror (errno));
+}
+
+int
+repeat (void)
+{
+    return 1;
+}
+
+char
+transform (char c)
+{
+    c = toupper (c);
+    if (!strchr(good_chars, c))
+        c = '\0';
+    if (c == '-')
+        c = ' ';
+    return c;
+}
+
+void
+load (const char *filename, char storage[SIZE][SIZE])
+{
+    FILE *pf;
+    char c;
+    int row = 0, col = 0;
+
+    memset(storage, ' ', SIZE*SIZE);
+
+    pf = fopen (filename, "r");
+    if (!pf)
+        error_ocurried ();
+
+    while (!feof (pf) )
+    {
+        c = fgets (pf);
+        if (c == '\n'){
+            col = 0;
+            row++;
+        }
+        c = transform (c);
+
+        if (c)
+            storage[row][col] = c;
+        col++;
+
+    }
+
+    fclose (pf);
+}
+
+void
+dump (char board[SIZE][SIZE])
+{
+    ;
+}
+
+void
+ask_coordinates (int *y, int *x, const char *name)
+{
+    ;
+}
---
+++ 16_chess/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "general.h"
+#include "io.h"
+
+int
+is_empty (int row, int col, char board[SIZE][SIZE])
+{
+    ;
+}
+
+int
+tower_check (int row, int col, char board[SIZE][SIZE])
+{
+    ;
+}
+
+int
+main (int argc, char *argv[])
+{
+    char chess_board[SIZE][SIZE];
+    int row, col;
+
+    do
+    {
+        loard (BOARD_FN, chess_board);
+        dump (chess_board);
+        do
+        {
+            ask_coordinates (&row, &col, "torre");
+        }
+        while (!is_empty (row, col, chess_board));
+        tower_check (row, col, chess_board);
+    }
+    while (repeat());
+
+	return EXIT_SUCCESS;
+}
